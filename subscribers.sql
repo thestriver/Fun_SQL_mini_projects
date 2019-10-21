b6fa77668e7ddab7dccd44b7a59a7b7cf680c903
thestriver <wannabeamumin@gmail.com>
2019-10-21 23:02:05 +0100

    new

--- subscribers.sql
+++ subscribers.sql
@@ -25,3 +25,23 @@ CROSS JOIN months
 WHERE start_month <= month 
    AND end_month >= month
 GROUP BY month;
+
+SELECT *
+FROM newspaper
+UNION
+SELECT *
+FROM online;
+
+
+WITH previous_query AS (
+SELECT customer_id,
+   COUNT(subscription_id) AS 'subscriptions'
+FROM orders
+GROUP BY customer_id
+)
+
+SELECT customers.customer_name, 
+   previous_query.subscriptions
+FROM previous_query
+JOIN customers
+  ON previous_query.customer_id = customers.customer_id;
